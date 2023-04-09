f189dbed0aa3c2b3c59ab3dbcfa3adf4e2333768
ashwathgowdanp <ashwathgowdanp@gmail.com>
2023-04-09 17:26:27 -0400

    commit of coffeee registrtion

---
+++ CoffeeShopRegistrationApp/CoffeeShopRegistration/Controllers/Registration.cs
@@ -0,0 +1,40 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class Registration : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+
+        public IActionResult HandleSubmit(UserProfile userProfile)
+        {
+            // We can process the UserProfile object now
+            // for example, we can save it do a database
+            if (!ModelState.IsValid)
+            {
+                return View(userProfile);
+            }
+            return RedirectToAction("Welcome", userProfile);
+        }
+
+        public IActionResult Welcome(UserProfile userProfile) 
+        { 
+            return View(userProfile);
+        }
+    }
+}
---
+++ CoffeeShopRegistrationApp/CoffeeShopRegistration/Models/UserProfile.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace CoffeeShopRegistration.Models
+{
+    public class UserProfile
+    {
+        [Required(ErrorMessage = "First name is required.")]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please enter your email.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter a valid password.")]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        [StringLength(16)]
+        public string Password { get; set; }
+    }
+}
